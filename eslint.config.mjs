9be5c459803228cc58b4e09023d697adc5a8204e
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2025-02-23 20:58:16 +0100

    Replace eslint-plugin-import by eslint-plugin-import-x

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,4 @@
-import eslintPluginImport from "eslint-plugin-import";
+import eslintPluginImport from "eslint-plugin-import-x";
 import eslintPluginJs from "@eslint/js";
 import eslintPluginJson from "@eslint/json";
 import eslintPluginMarkdown from "@eslint/markdown";
