7396d411a068c58bce71cf1bc6dbd358a3bf0121
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2025-03-12 21:28:23 +0100

    chore: simplify stylelint-prettier config

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,9 +1,7 @@
 const config = {
-  "extends": ["stylelint-config-standard"],
-  "plugins": ["stylelint-prettier"],
+  "extends": ["stylelint-config-standard", "stylelint-prettier/recommended"],
   "root": true,
   "rules": {
-    "prettier/prettier": true,
     "selector-class-pattern": [
       "^(?:[a-z]+(-[a-z]+)*|MMM-Forum)$",
       {
